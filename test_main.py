341dfe681f8de2f5e2f80c52aa008dbdf46c4974
Nashid Mateen <mrnashidmateen@gmail.com>
2024-09-22 07:53:12 -0700

    Second Set of Changes

--- test_main.py
+++ test_main.py
@@ -1,9 +1,14 @@
 import unittest
-from main import hello_world
+from main import hello_world, greet_person, name
+
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(hello_world(), "Hello, from Nashid!")
+
+class TestGreeting(unittest.TestCase):
+    def test_greeting(self):
+        self.assertEqual(greet_person(name), f"Hello, {name}!")
 
 if __name__ == '__main__':
     unittest.main()
